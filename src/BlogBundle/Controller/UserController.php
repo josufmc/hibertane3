1affdf657ba12b5cd7441f6cbc2500d199342fab
Josu <josufmc@hotmail.com>
2016-11-06 14:41:47 +0100

    Added flash messages to register form

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -6,28 +6,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use BlogBundle\Entity\User;
 use BlogBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Session\Session;
 
-class UserController extends Controller
-{
-    public function loginAction(Request $request){
+class UserController extends Controller {
+
+    private $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+
+    public function loginAction(Request $request) {
         $authenticationUtils = $this->get("security.authentication_utils");
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUserName();
-        
+
         $user = new User();
         $form = $this->createForm(Usertype::class, $user);
         $form->handleRequest($request);
-        if ($form->isValid()){
-            $status = "El usuario se ha creado correctamente";
-        } else {
-            $status = "No te has registrado correctamente";
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $user = new User();
+                $user->setName($form->get('name')->getData());
+                $user->setSurname($form->get('surname')->getData());
+                $user->setEmail($form->get('email')->getData());
+                $user->setPassword($form->get('password')->getData());
+                $user->setRole('ROLE_USER');
+                $user->setImagen(null);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $flush = $em->flush();
+
+                if ($flush == null) {
+                    $status = "El usuario se ha creado correctamente";
+                } else {
+                    $status = "No te has registrado correctamente";
+                }
+            } else {
+                $status = "No te has registrado correctamente";
+            }
+            $this->session->getFlashBag()->add('status', $status);
         }
-        
+
         return $this->render('BlogBundle:User:login.html.twig', array(
-            "error" => $error,
-            "last_username" => $lastUsername,
-            "form" => $form->createView()
+                    "error" => $error,
+                    "last_username" => $lastUsername,
+                    "form" => $form->createView()
         ));
     }
-    
+
 }
--- src/BlogBundle/Form/UserType.php
+++ src/BlogBundle/Form/UserType.php
@@ -5,22 +5,34 @@ namespace BlogBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class UserType extends AbstractType
-{
+class UserType extends AbstractType {
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('name')->add('surname')->add('email')->add('password')        ;
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('name', TextType::class, array('required' => 'required', 'label' => 'Nombre',
+                    'attr' => array('class' => 'form-name form-control')))
+                ->add('surname', TextType::class, array('required' => 'required', 'label' => 'Apellidos',
+                    'attr' => array('class' => 'form-surname form-control')))
+                ->add('email', EmailType::class, array('required' => 'required', 'label' => 'Email',
+                    'attr' => array('class' => 'form-email form-control')))
+                ->add('password', PasswordType::class, array('required' => 'required', 
+                    'attr' => array('class' => 'form-password form-control')))
+                ->add('Guardar', SubmitType::class, array(
+                    'attr' => array('class' => 'btn btn-success')))
+        ;
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'BlogBundle\Entity\User'
         ));
@@ -29,10 +41,8 @@ class UserType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'blogbundle_user';
     }
-
 
 }
